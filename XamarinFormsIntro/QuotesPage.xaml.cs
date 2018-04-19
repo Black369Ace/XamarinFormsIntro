aa477c2b50a22fc4503a1e13787170268910b007
Black369Ace <black369ace@gmail.com>
2018-04-20 08:49:34 +1000

    Initial commit

---
+++ XamarinFormsIntro/QuotesPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsIntro
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QuotesPage : ContentPage
+	{
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+        }
+
+
+        static int FontSize = 5;
+
+
+            List<string> school = new List<string>()
+            {
+                "F is for friends who do stuff together",
+                "U is for you and me",
+                "N is for anywhere, anytime at all",
+                "Down here in the deep blue sea!"
+            };
+
+
+            Label rotationLabel = new Label
+            {
+                Text = "Font Size" + FontSize;
+            }
+	}
+}
